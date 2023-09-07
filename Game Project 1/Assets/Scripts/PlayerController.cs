bdf15f7d45abb34ef16c980678306e1543305986
JustWinBlyat <adam.saleh2610@gmail.com>
2023-09-07 17:20:37 -0400

    Created a MovementFunctions Class
    Created a class that acts as a library for all movement types that allow us to call the function of the class into our PlayerController script

--- Game Project 1/Assets/Scripts/PlayerController.cs
+++ Game Project 1/Assets/Scripts/PlayerController.cs
@@ -33,14 +33,8 @@ public class PlayerController : MonoBehaviour
     private KeyCode moveRight = KeyCode.D;
     [SerializeField]
     private KeyCode sprintKey = KeyCode.LeftShift;
-    [SerializeField]
-    private KeyCode lockState = KeyCode.Escape;
-
-    [Space]
 
     //States:
-    [SerializeField]
-    private bool isCursorLocked = true; //A boolean that tracks the mouse cursor state
 
     void Start()
     {
@@ -51,7 +45,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMovement();
-        MouseCursorState();
     }
 
     //This is the player movement method. PLEASE DON'T TOUCH!!!!!!!!!!!
@@ -60,93 +53,31 @@ public class PlayerController : MonoBehaviour
         //Move Forward:
         if (Input.GetKey(moveForward))
         {
-            MoveForward();
+            MovementFunctions.MoveForward(transform, walkingSpeed);
         }
 
         //Move Backward:
         if (Input.GetKey(moveBackward))
         {
-            MoveBackward();
+            MovementFunctions.MoveBackward(transform, walkingSpeed);
         }
 
         //Move Left:
         if (Input.GetKey(moveLeft))
         {
-            MoveLeft();
+            MovementFunctions.MoveLeft(transform, strafeSpeed);
         }
 
         //Move Right:
         if (Input.GetKey(moveRight))
         {
-            MoveRight();
+            MovementFunctions.MoveRight(transform, strafeSpeed);
         }
 
         //Sprint Forward:
         if (Input.GetKey(moveForward) && Input.GetKey(sprintKey))
         {
-            SprintForward();
+            MovementFunctions.SprintForward(transform, sprintSpeed);
         }
-    }
-
-    
-
-    //This is a cursor function that controls the cursor visibility during gameplay
-    void MouseCursorState()
-    {
-        if (Input.GetKey(lockState))
-        {
-            isCursorLocked = !isCursorLocked;
-
-            if (isCursorLocked)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-
-            else
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-        }
-    }
-
-    /************************
-    Movement Functions Here:
-    - Forward
-    - Backward
-    - Side (Left/Right)
-    - Sprint
-    - Jump
-    *************************/
-
-    void MoveForward()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * walkingSpeed);
-    }
-
-    void MoveBackward()
-    {
-        transform.Translate(Vector3.back * Time.deltaTime * walkingSpeed);
-    }
-
-    void MoveLeft()
-    {
-        transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed);
-    }
-
-    void MoveRight()
-    {
-        transform.Translate(Vector3.right * Time.deltaTime * strafeSpeed);
-    }
-
-    void SprintForward()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * sprintSpeed);
-    }
-
-    void Jump()
-    {
-        //Jumping Logic Here Sir!
     }
 }
