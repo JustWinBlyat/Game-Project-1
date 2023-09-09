5205a551d0dfb0621da8528974b02f1530a986ef
JustWinBlyat <adam.saleh2610@gmail.com>
2023-09-09 12:44:04 -0400

    Added a rigidbody force motion

--- Game Project 1/Assets/Scripts/PlayerController.cs
+++ Game Project 1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Player Movement
+    [SerializeField]
+    private float speed = 5.0f;
+    //Player Component
+    private Rigidbody playerRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetRigidbody();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveVertical();
+    }
+
+    //Gets the object's rigidbody component and calls it to the Start() method.
+    void GetRigidbody()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void MoveVertical()
+    {
+        //Make sure the input manager name PlayerVertical is found.
+        //This is meant to separate the Camera's movement binding from the player's.
+        float forwardInput = Input.GetAxis("PlayerVertical");
+    
+        playerRigidbody.AddForce(Vector3.forward * forwardInput * speed);
     }
 }
