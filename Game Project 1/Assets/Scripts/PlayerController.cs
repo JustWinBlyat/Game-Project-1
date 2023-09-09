cd7b47f8bcf493006598619d0f40587571635089
JustWinBlyat <adam.saleh2610@gmail.com>
2023-09-09 12:29:34 -0400

    Reworking PlayerController

--- Game Project 1/Assets/Scripts/PlayerController.cs
+++ Game Project 1/Assets/Scripts/PlayerController.cs
@@ -4,85 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Here are all the variables for our PlayerController Class:
-
-    //Movement:
-    [SerializeField]
-    private float walkingSpeed = 1.5f; //A speed variable that allows us to change the player's walking speed
-    [SerializeField]
-    private float sprintSpeed = 3f; //A speed variable that allows us to change the players spriting speed
-    [SerializeField]
-    public float moveSpeed = 5f;  //A move variable that allows us to change the players speed in relation to the camera POV
-    [SerializeField]
-    private float strafeSpeed = 1f; //A speed variable that allows us to change the player's side movement
-    [SerializeField]
-    private float jumpForce = 5.0f; //A jump force variable that allows us to change the player's jump strength
-
-    [Space]
-
-    //Keybinds:
-    [SerializeField]
-    private KeyCode jump = KeyCode.Space;
-    [SerializeField]
-    private KeyCode moveForward = KeyCode.W;
-    [SerializeField]
-    private KeyCode moveBackward = KeyCode.S;
-    [SerializeField]
-    private KeyCode moveLeft = KeyCode.A;
-    [SerializeField]
-    private KeyCode moveRight = KeyCode.D;
-    [SerializeField]
-    private KeyCode sprintKey = KeyCode.LeftShift;
-
-    //States:
-
+    // Start is called before the first frame update
     void Start()
     {
-        //Making sure the game starts
-        Debug.Log("Game has begun!");
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        PlayerMovement();
-    }
-
-    //This is the player movement method. PLEASE DON'T TOUCH!!!!!!!!!!!
-    void PlayerMovement()
-    {
-        //Move Forward:
-        if (Input.GetKey(moveForward))
-        {
-            MovementFunctions.MoveForward(transform, walkingSpeed);
-        }
-
-        //Move Backward:
-        if (Input.GetKey(moveBackward))
-        {
-            MovementFunctions.MoveBackward(transform, walkingSpeed);
-        }
-
-        //Move Left:
-        if (Input.GetKey(moveLeft))
-        {
-            MovementFunctions.MoveLeft(transform, strafeSpeed);
-        }
-
-        //Move Right:
-        if (Input.GetKey(moveRight))
-        {
-            MovementFunctions.MoveRight(transform, strafeSpeed);
-        }
-
-        //Sprint Forward:
-        if (Input.GetKey(moveForward) && Input.GetKey(sprintKey))
-        {
-            MovementFunctions.SprintForward(transform, sprintSpeed);
-        }
-
-        //if (Input.GetKey(jump))
-        //{
-        //    MovementFunctions.Jump(transform, jumpForce);
-        //}
+        
     }
 }
