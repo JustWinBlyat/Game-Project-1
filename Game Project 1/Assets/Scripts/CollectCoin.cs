7056945ada9f18c041a220a98ff7322bc1e06b12
JustWinBlyat <adam.saleh2610@gmail.com>
2023-10-04 22:06:12 -0400

    Updated the CollectCoin script

--- Game Project 1/Assets/Scripts/CollectCoin.cs
+++ Game Project 1/Assets/Scripts/CollectCoin.cs
@@ -15,4 +15,12 @@ public class CollectCoin : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
