d87bacbac7372ccd4bd67884ff8a459e588cef51
JustWinBlyat <adam.saleh2610@gmail.com>
2023-10-04 22:02:09 -0400

    Fixed rotateAround script

--- Game Project 1/Assets/Scripts/Diamond.cs
+++ Game Project 1/Assets/Scripts/Diamond.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Diamond : MonoBehaviour
 {
+    [SerializeField]
+    private float rotationSpeed = 30f;
+
     // Start is called before the first frame update
     void Start()
     {
+        transform.position = new Vector3(0f, 5f, 0f);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
     }
 }
